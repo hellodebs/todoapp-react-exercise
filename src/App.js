eef0219abca24e1fdebdd5ca7d208605f87414ba
hellodebs <debora.behrens@outlook.com>
2021-07-14 21:18:20 +0200

    Add input and button element to app.js, note three components of app, create partially static version in React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import "./App.css";
 
-function App() {
+export default function App() {
+  const toDoListItems = [
+    {
+      todo: "workout", //string
+      isDone: false, //boolean
+    },
+    {
+      todo: "massage",
+      isDone: true,
+    },
+    {
+      todo: "homework",
+      isDone: false,
+    },
+  ];
+  //console.log(toDoListItems);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>To-Do App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="My to-do..." />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+/* 3 Components: 
+1. TodoTable: contains the whole app.
+2. InputBar: receives all ToDo items.
+3. ItemTable: displays all inserted ToDos.
+*/
